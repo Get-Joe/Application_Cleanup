ffa211583fa9ddc39aa20ee6073ec6ec27a6da64
get-joe <sithlord1977@gmail.com>
2018-03-02 14:03:59 -0500

    cleanup

--- Filter.sh
+++ Filter.sh
@@ -5,5 +5,10 @@
 # grep -o '\rc.* ' - selects only the lines that start with rc.
 # awk '{print $2}' - prints the second colomn.
 # > RemoveApps.txt - overwrites the file RemoveApps.txt used later to input for the apt-get purge
+# this is part of the full script, having it seperate can be usefull to pull information on the system.
+
+set-e
 
 dpkg -l | grep -v '^ii' | grep -o '\rc.* ' | awk '{print $2}' > RemoveApps.txt
+
+exit 0
