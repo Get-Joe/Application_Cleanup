6fab43f531203253618fdfdd701402986a3cfc34
get-joe <sithlord1977@gmail.com>
2018-03-02 12:08:27 -0500

    cleanup

--- Filter.sh
+++ Filter.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
+
+# dpkg -l - list all items in dpkg
+# grep -v '^ii' - 
+
 dpkg -l | grep -v '^ii' | grep -o '\rc.* ' | awk '{print $2}' > RemoveApps.txt
--- PurgeBrokenApps.sh
+++ PurgeBrokenApps.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
+set -e
+
 dpkg -l | grep -v '^ii' | grep -o '\rc.* ' | awk '{print $2}' > RemoveApps.txt
 
 for APPLICATION in `cat RemoveApps.txt`
 do
-apt purge $APPLICATION
+apt-get purge $APPLICATION
 done
+
 exit 0
